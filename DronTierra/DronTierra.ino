db7402881065b9646a916d35a6b878cf880074a7
Edgar Sánchez <esanchez@sigma.cl>
2017-03-28 01:06:38 -0400

    more

--- DronTierra/DronTierra.ino
+++ DronTierra/DronTierra.ino
@@ -4,12 +4,23 @@
 #include <ESP8266mDNS.h>
 #include <ArduinoJson.h>
 
+//Derecha
+static int PinOUTA   = 5; // (D1)
+static int PinOUTB   = 4; // (D2)
+
+//Izquierda
+static int PinOUTC   = 0; // (D3)
+static int PinOUTD   = 2; // (D4)
+
+
 //const char* ssid = "yai";
 //const char* password = "1101000000";
 
 const char* ssid = "VTR-YAI-5Ghz";
 const char* password = "Pana8bc1108";
 String input = "{\"sensor\":\"gps\",\"time\":1351824120,\"data\":[48.756080,2.302038]}";
+String ipESP = "";
+int count= 0;
 DynamicJsonBuffer jsonBuffer;
 
 ESP8266WebServer server(80);
@@ -39,9 +50,14 @@ void handleNotFound(){
   digitalWrite(led, 0);
 }
 
-void setup(void){
-  pinMode(led, OUTPUT);
-  digitalWrite(led, 0);
+void setup(void){  
+  pinMode(LED_BUILTIN, OUTPUT); 
+  pinMode(PinOUTA, OUTPUT);
+  pinMode(PinOUTB, OUTPUT);
+  pinMode(PinOUTC, OUTPUT);
+  pinMode(PinOUTD, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
+  digitalWrite(led, 1);
   Serial.begin(115200);
   WiFi.begin(ssid, password);
   Serial.println("");
@@ -63,8 +79,38 @@ void setup(void){
 
   server.on("/", handleRoot);
 
-  server.on("/inline", [](){
-    String message = "this works as well\n\n";
+  server.on("/move", [](){
+    
+    digitalWrite(LED_BUILTIN, LOW);        
+    if(server.args()>0){
+      if(server.argName(0) == "CMD"){
+        server.send(200, "text/plain", "MOVE >> " + server.arg(0)); 
+        if(server.arg(0) == "FORWARD"){
+          MotorAntihorario(PinOUTA, PinOUTB);
+          MotorAntihorario(PinOUTC, PinOUTD);
+        }
+        if(server.arg(0) == "BACK"){
+          MotorHorario(PinOUTA, PinOUTB);
+          MotorHorario(PinOUTC, PinOUTD);          
+        }        
+        if(server.arg(0) == "LEFT"){
+          MotorAntihorario(PinOUTA, PinOUTB);
+          digitalWrite (PinOUTC, LOW);
+          digitalWrite (PinOUTD, LOW);     
+        }        
+        if(server.arg(0) == "RIGHT"){
+          MotorAntihorario(PinOUTC, PinOUTD);
+          digitalWrite (PinOUTA, LOW);
+          digitalWrite (PinOUTB, LOW);     
+        }        
+      }
+    }
+  });
+
+  server.on("/stop", [](){
+    MotorStop();
+    digitalWrite(LED_BUILTIN, HIGH);    
+    String message = "LED OFF \n\n";
     message += "\nArguments: ";
     message += server.args();
     message += "\n";
@@ -73,6 +119,7 @@ void setup(void){
     String output;
     root.printTo(output);
     Serial.print(output);
+    Serial.print("\n");
     for (uint8_t i=0; i<server.args(); i++){
       message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
     }    
@@ -83,8 +130,33 @@ void setup(void){
 
   server.begin();
   Serial.println("HTTP server started");
+  count = 1;
 }
 
 void loop(void){
   server.handleClient();
+  if (count == 1){
+    ipESP = WiFi.localIP().toString();
+    Serial.println("HTTP server listen on ip "  + ipESP);
+    count = 2;
+  }
+}
+
+void MotorHorario(int pin1, int pin2)
+{
+  digitalWrite (pin1, HIGH);
+  digitalWrite (pin2, LOW);
+}
+void MotorAntihorario(int pin1, int pin2)
+{
+  digitalWrite (pin1, LOW);
+  digitalWrite (pin2, HIGH);
+}
+
+void MotorStop()
+{
+  digitalWrite (PinOUTA, LOW);
+  digitalWrite (PinOUTB, LOW);
+  digitalWrite (PinOUTC, LOW);
+  digitalWrite (PinOUTD, LOW);
 }
