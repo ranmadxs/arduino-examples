c691c0f8bc4f33b23f348291eeb4e572ccbc1cdc
Edgar Sánchez <esanchez@sigma.cl>
2017-04-18 00:07:43 -0400

    ip

--- DronTierra/YaiJS.cpp
+++ DronTierra/YaiJS.cpp
@@ -1,12 +1,14 @@
 #include "YaiOS.h"
+#include "YaiConstants.h"
 
 String YaiOS::getJS(){
   String html = 
   "\
+  var ipClient = '"+ YaiOS::getClientIP()+"';\  
   function actionMove(move) { \
     alert('Move:'+move);\
     var xhttp = new XMLHttpRequest();\
-	xhttp.open('GET', 'http://192.168.100.101/cmd?COMMAND=100001&P1=1001&P2=0&P3=0&P4=2003&P5=None&P6=None&P7=None', false);\
+	xhttp.open('GET', 'http://'+ipClient+'/cmd?COMMAND=100001&P1=1001&P2=0&P3=0&P4=2003&P5=None&P6=None&P7=None', false);\
     xhttp.setRequestHeader('Content-type', 'text/html');\
 	xhttp.send();\
 	alert('XDDDD');\
