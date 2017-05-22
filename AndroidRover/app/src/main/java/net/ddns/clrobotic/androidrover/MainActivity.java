66d30e2683d40fe1d4305e0f488c69e6404843ed
edgar.sanchez <esanchez@redtogreen.com>
2017-05-22 18:56:56 -0400

    Agregando btn

--- AndroidRover/app/src/main/java/net/ddns/clrobotic/androidrover/MainActivity.java
+++ AndroidRover/app/src/main/java/net/ddns/clrobotic/androidrover/MainActivity.java
@@ -2,12 +2,26 @@ package net.ddns.clrobotic.androidrover;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnForward;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i("MainActivity", "onCreate...");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnForward = (Button)findViewById(R.id.btn_forward);
+        btnForward.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i("MainActivity", "apretaste btn forward");
+            }
+        });
+
     }
 }
