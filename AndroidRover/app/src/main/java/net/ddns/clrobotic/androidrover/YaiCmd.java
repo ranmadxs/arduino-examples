120460d468dd95674c95208394751cceb79ff56b
edgar.sanchez <esanchez@redtogreen.com>
2017-05-23 15:15:26 -0400

    YaiCmd

---
+++ AndroidRover/app/src/main/java/net/ddns/clrobotic/androidrover/YaiCmd.java
@@ -0,0 +1,82 @@
+package net.ddns.clrobotic.androidrover;
+
+/**
+ * Created by esanchez on 23-05-2017.
+ */
+
+public class YaiCmd {
+
+    public String command;
+    public String p1;
+    public String p2;
+    public String p3;
+    public String p4;
+    public String p5;
+    public String p6;
+    public String p7;
+
+
+    public String getCommand() {
+        return command;
+    }
+
+    public void setCommand(String command) {
+        this.command = command;
+    }
+
+    public String getP1() {
+        return p1;
+    }
+
+    public void setP1(String p1) {
+        this.p1 = p1;
+    }
+
+    public String getP2() {
+        return p2;
+    }
+
+    public void setP2(String p2) {
+        this.p2 = p2;
+    }
+
+    public String getP3() {
+        return p3;
+    }
+
+    public void setP3(String p3) {
+        this.p3 = p3;
+    }
+
+    public String getP4() {
+        return p4;
+    }
+
+    public void setP4(String p4) {
+        this.p4 = p4;
+    }
+
+    public String getP5() {
+        return p5;
+    }
+
+    public void setP5(String p5) {
+        this.p5 = p5;
+    }
+
+    public String getP6() {
+        return p6;
+    }
+
+    public void setP6(String p6) {
+        this.p6 = p6;
+    }
+
+    public String getP7() {
+        return p7;
+    }
+
+    public void setP7(String p7) {
+        this.p7 = p7;
+    }
+}
