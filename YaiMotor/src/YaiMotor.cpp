a716350fa7026d01b86f1f71144295721eda43a7
Edgar Sánchez <esanchez@sigma.cl>
2017-05-25 02:11:40 -0400

    quitando comentarios

--- YaiMotor/src/YaiMotor.cpp
+++ YaiMotor/src/YaiMotor.cpp
@@ -7,27 +7,6 @@
 RoverLink roverLn;
 ObstacleLink obstacleLn;
 
-//TODO: Debo probar esto
-//char *strtok_r(char *str, const char *delim, char **save)
-//{
-//    char *res, *last;
-//
-//    if( !save )
-//        return strtok(str, delim);
-//    if( !str && !(str = *save) )
-//        return NULL;
-//    last = str + strlen(str);
-//    if( (*save = res = strtok(str, delim)) )
-//    {
-//        *save += strlen(res);
-//        if( *save < last )
-//            (*save)++;
-//        else
-//            *save = NULL;
-//    }
-//    return res;
-//}
-
 YaiUtil yaiUtil;
 
 void serialController(){
