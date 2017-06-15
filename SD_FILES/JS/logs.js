c5d1a133d086b342bab5b29812f495c08fef1d0d
edgar.sanchez <esanchez@redtogreen.com>
2017-06-14 20:14:49 -0400

    logs

--- SD_FILES/JS/logs.js
+++ SD_FILES/JS/logs.js
@@ -1,6 +1,9 @@
 var TIPO_CALL = "SERIAL";
 var ipClient = "${clientIp}"; 
-var ipClient = "192.168.0.23";
+var ipClient = "192.168.100.103";
+var init = 1;
+var end = 4;
+
 function actCmd(cmd, p1, p2, p3, p4, p5, p6, p7) { 
 	p1 = p1 || 'None'; 
 	p2 = p2 || 'None'; 
@@ -9,33 +12,25 @@ function actCmd(cmd, p1, p2, p3, p4, p5, p6, p7) {
 	p5 = p5 || 'None'; 
 	p6 = p6 || 'None'; 
 	p7 = p7 || 'None'; 
-	alert("Enviando data v222");
-	//$.get( 'http://'+ipClient+'/cmd', 		
-	//	{ TIPO_CALL: TIPO_CALL, COMMAND: cmd, P1: p1, P2: p2, P3: p3, P4: p4, P5: p5, P6: p6, P7: p7 }, function( data ) {
-	//	alert("Resultado del envio:::");
-	//	alert(data);
-	//} );	
-	$.ajax({
-		method: "GET",
-		url: 'http://'+ipClient+'/cmd',
-		data: { TIPO_CALL: TIPO_CALL, COMMAND: cmd, P1: p1, P2: p2, P3: p3, P4: p4, P5: p5, P6: p6, P7: p7 },
-		dataType: "jsonp",
-		success: function(result){
-			alert("XDDDDD");
-		}
-	});
 	
-    //var xhttp = new XMLHttpRequest();
-	//xhttp.open('GET', 'http://'+ipClient+'/cmd?TIPO_CALL='+TIPO_CALL+'&COMMAND='+cmd+'&P1='+p1+'&P2='+p2+'&P3='+p3+'&P4='+p4+'&P5='+p5+'&P6='+p6+'&P7='+p7, false);
-    //xhttp.setRequestHeader('Content-type', 'text/html');
-	//var result = xhttp.send();
-	//alert("Result:");
-	//alert(result);	
+	var url ='http://'+ipClient+'/cmd?TIPO_CALL='+TIPO_CALL+'&COMMAND='+cmd+'&P1='+p1+'&P2='+p2+'&P3='+p3+'&P4='+p4+'&P5='+p5+'&P6='+p6+'&P7='+p7;
+	alert(url);
+	document.getElementById("iframeLogs").contentDocument.location.reload(true);
 }
 
 $( document ).ready(function() {
-	setInterval(function(){ 
-		//alert("Hello"); 
-		actCmd("100005", 1, 12, true);
-	}, 3000);
+	$("#siguiente").click(function(){
+		init = end;
+		end = end + 4;
+		actCmd('100005', init, end);		
+	});
+	$("#anterior").click(function(){
+		end = init;
+		init = init - 4;
+		if (init < 1){
+			init = 1;
+			end = 4;
+		}
+		actCmd('100005', init, end);		
+	});	
 });
