980a1d1f3a3b433cf1d89b775263b494a7ba25a5
Edgar Sanchez <grineldosanchez@yahoo.es>
2017-10-30 01:09:27 -0300

    test

--- python/ReconocimientoImgs/04test.py
+++ python/ReconocimientoImgs/04test.py
@@ -1,135 +1,62 @@
-import cv2
+#------------Opencv 2.7 used ------------#
 import numpy as np
-import copy
-import math
-from appscript import app
-
-# Environment:
-# OS    : Mac OS EL Capitan
-# python: 3.5
-# opencv: 2.4.13
-
-# parameters
-cap_region_x_begin=0.5  # start point/total width
-cap_region_y_end=0.8  # start point/total width
-threshold = 60  #  BINARY threshold
-blurValue = 41  # GaussianBlur parameter
-bgSubThreshold = 50
-
-# variables
-isBgCaptured = 0   # bool, whether the background captured
-triggerSwitch = False  # if true, keyborad simulator works
-
-def printThreshold(thr):
-    print("! Changed threshold to "+str(thr))
-
-
-def removeBG(frame):
-    fgmask = bgModel.apply(frame)
-    # kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
-    # res = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel)
-
-    kernel = np.ones((3, 3), np.uint8)
-    fgmask = cv2.erode(fgmask, kernel, iterations=1)
-    res = cv2.bitwise_and(frame, frame, mask=fgmask)
-    return res
-
-
-def calculateFingers(res,drawing):  # -> finished bool, cnt: finger count
-    #  convexity defect
-    hull = cv2.convexHull(res, returnPoints=False)
-    if len(hull) > 3:
-        defects = cv2.convexityDefects(res, hull)
-        if type(defects) != type(None):  # avoid crashing.   (BUG not found)
-
-            cnt = 0
-            for i in range(defects.shape[0]):  # calculate the angle
-                s, e, f, d = defects[i][0]
-                start = tuple(res[s][0])
-                end = tuple(res[e][0])
-                far = tuple(res[f][0])
-                a = math.sqrt((end[0] - start[0]) ** 2 + (end[1] - start[1]) ** 2)
-                b = math.sqrt((far[0] - start[0]) ** 2 + (far[1] - start[1]) ** 2)
-                c = math.sqrt((end[0] - far[0]) ** 2 + (end[1] - far[1]) ** 2)
-                angle = math.acos((b ** 2 + c ** 2 - a ** 2) / (2 * b * c))  # cosine theorem
-                if angle <= math.pi / 2:  # angle less than 90 degree, treat as fingers
-                    cnt += 1
-                    cv2.circle(drawing, far, 8, [211, 84, 0], -1)
-            return True, cnt
-    return False, 0
-
-
-# Camera
-camera = cv2.VideoCapture(0)
-camera.set(10,200)
-cv2.namedWindow('trackbar')
-cv2.createTrackbar('trh1', 'trackbar', threshold, 100, printThreshold)
-
-
-while camera.isOpened():
-    ret, frame = camera.read()
-    threshold = cv2.getTrackbarPos('trh1', 'trackbar')
-    frame = cv2.bilateralFilter(frame, 5, 50, 100)  # smoothing filter
-    frame = cv2.flip(frame, 1)  # flip the frame horizontally
-    cv2.rectangle(frame, (int(cap_region_x_begin * frame.shape[1]), 0),
-                 (frame.shape[1], int(cap_region_y_end * frame.shape[0])), (255, 0, 0), 2)
-    cv2.imshow('original', frame)
-
-    #  Main operation
-    if isBgCaptured == 1:  # this part wont run until background captured
-        img = removeBG(frame)
-        img = img[0:int(cap_region_y_end * frame.shape[0]),
-                    int(cap_region_x_begin * frame.shape[1]):frame.shape[1]]  # clip the ROI
-        cv2.imshow('mask', img)
-
-        # convert the image into binary image
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        blur = cv2.GaussianBlur(gray, (blurValue, blurValue), 0)
-        cv2.imshow('blur', blur)
-        ret, thresh = cv2.threshold(blur, threshold, 255, cv2.THRESH_BINARY)
-        cv2.imshow('ori', thresh)
-
-
-        # get the coutours
-        thresh1 = copy.deepcopy(thresh)
-        contours, hierarchy = cv2.findContours(thresh1, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        length = len(contours)
-        maxArea = -1
-        if length > 0:
-            for i in range(length):  # find the biggest contour (according to area)
-                temp = contours[i]
-                area = cv2.contourArea(temp)
-                if area > maxArea:
-                    maxArea = area
-                    ci = i
-
-            res = contours[ci]
-            hull = cv2.convexHull(res)
-            drawing = np.zeros(img.shape, np.uint8)
-            cv2.drawContours(drawing, [res], 0, (0, 255, 0), 2)
-            cv2.drawContours(drawing, [hull], 0, (0, 0, 255), 3)
-
-            isFinishCal,cnt = calculateFingers(res,drawing)
-            if triggerSwitch is True:
-                if isFinishCal is True and cnt <= 2:
-                    print cnt
-                    app('System Events').keystroke(' ')  # simulate pressing blank space
-
-        cv2.imshow('output', drawing)
-
-    # Keyboard OP
-    k = cv2.waitKey(10)
-    if k == 27:  # press ESC to exit
+import cv2
+lo = np.array([0,130,101])
+hi = np.array([198,155,148])
+kernel2 = np.ones((3,3),np.uint8)
+kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(15,15))
+kodi = np.ones((480,640,3),np.uint8)
+found = 0
+def putText(cv2,frame,txt):
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    #cv2.putText(frame,'Fingers:'+txt,(0,70), font, 1,(255,255,255),2,cv2.CV_AA)
+    cv2.putText(frame,'Fingers:'+txt, (0,70), font, 2, 255)
+#------------video capture from external camera(pass 1) or pass 0 for inbuilt camera-----------#
+cap = cv2.VideoCapture(0)
+while(1):
+    ret, frame = cap.read()
+    # ------ YCrCb Color space part--------#
+    yc = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    #yc = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(yc,lo,hi)
+    mask = cv2.GaussianBlur(mask,(0,0),2)
+    mask = cv2.morphologyEx(mask,cv2.MORPH_CLOSE,kernel2,iterations = 2)
+    mask3 = cv2.bitwise_and(frame,frame,mask=mask)
+    #------- Threshold and Contour extraction ---------------- #
+    ret,th = cv2.threshold(mask,60,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    thh = th.copy()
+    imageCountours, cnt, hie = cv2.findContours(th,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+    cov = len(cnt)
+    numb = np.array([])
+    if(cov>0):
+        for i in range(cov):
+            covt = cnt[i]
+            ar = cv2.contourArea(covt)
+            if(ar>230):
+                M = cv2.moments(covt)
+                x1 = int(M['m10']/M['m00'])
+                y1 = int(M['m01']/M['m00'])
+                centroid = (x1,y1)
+                numb = np.append(numb,x1)
+                cv2.circle(frame,(x1,y1),3,(0,24,206),3)
+                x,y,w,h = cv2.boundingRect(covt)
+#------------------------ hull and defects -----------------#
+                try:
+                    hull = cv2.convexHull(covt,returnPoints = False)
+                    defects = cv2.convexityDefects(covt,hull)
+                    for i in range(defects.shape[0]):
+                        s,e,f,d = defects[i,0]
+                        start = tuple(covt[s][0])
+                        end = tuple(covt[e][0])
+                        far = tuple(covt[f][0])
+                except:
+                    continue
+#------------------------ hull and defects END-----------------#
+        cv2.drawContours(frame,cnt,-1,(34,223,109),3)
+    putText(cv2,frame,str(numb.shape[0]))
+    cv2.imshow('kovi',mask3)
+    cv2.imshow('Hand detec',frame)
+    if(cv2.waitKey(20) == 27):
         break
-    elif k == ord('b'):  # press 'b' to capture the background
-        bgModel = cv2.BackgroundSubtractorMOG2(0, bgSubThreshold)
-        isBgCaptured = 1
-        print '!!!Background Captured!!!'
-    elif k == ord('r'):  # press 'r' to reset the background
-        bgModel = None
-        triggerSwitch = False
-        isBgCaptured = 0
-        print '!!!Reset BackGround!!!'
-    elif k == ord('n'):
-        triggerSwitch = True
-        print '!!!Trigger On!!!'
+cap.release()
+cv2.destroyAllWindows()
